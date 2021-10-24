a3bb00c7da51a8de300a943b343471d70292b7a8
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-10-24 19:30:28 +0100

    implemented get and post comments for comment microservice

--- comments/src/app.js
+++ comments/src/app.js
@@ -1,12 +1,34 @@
-const express = require("express")
+const express = require("express");
+const { randomBytes } = require("crypto");
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.use(express.json())
+// in-memory storage
+const commentsByPostId = {};
 
-app.get("/", (req, res) => {
-  return res.send("welcome to comments microservices...")
-})
+app.get("/posts/:id/comments", (req, res) => {
+  return res.json(commentsByPostId[req.params.id] || []);
+});
 
+app.post("/posts/:id/comments", (req, res) => {
+  const id = randomBytes(5).toString("hex");
+  const { content } = req.body;
+  const postId = req.params.id;
 
-module.exports = app
+  if (!content) {
+    return res.status(400).json({ message: "content field required" });
+  }
+  let comments = commentsByPostId[postId] || [];
+  if (comments.length > 0) {
+    const isExist = comments.filter((comment) => comment.content === content);
+    if (isExist) {
+      return res.status(400).json({ message: "comment already exists" });
+    }
+  }
+  comments = [...comments, { id, content }];
+  commentsByPostId[postId] = comments;
+  return res.json(comments);
+});
+
+module.exports = app;
