44685569bbaa27cf0f40452861631b66872e3f87
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-10-25 15:43:50 +0100

    bootstrapped query server

--- event-bus/src/app.js
+++ event-bus/src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
+app.use(express.json());
 
 app.post("/event", async (req, res) => {
   const data = req.body;
