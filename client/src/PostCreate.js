84850d0f309b9543b75f64f8b928b825722b834a
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-10-25 05:32:54 +0100

    code refactoring of frontend

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { URL } from "./constant";
 
 const PostCreate = () => {
   const [title, setTitle] = useState("");
@@ -7,7 +8,7 @@ const PostCreate = () => {
   const onSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.post("http://localhost:4000/posts", {
+    await axios.post(`${URL}/posts`, {
       title,
     });
 
