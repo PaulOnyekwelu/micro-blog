3c323ce5a32bd455538a8883e7157c819208da1a
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-10-25 15:40:38 +0100

    bootstrapped event-bus server

---
+++ event-bus/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+
+app.post("/event", async (req, res) => {
+  const data = req.body;
+
+  const resp = await Promise.all([
+    axios.post("http://localhost:4000", data),
+    axios.post("http://localhost:4001", data),
+    axios.post("http://localhost:4002", data),
+  ]);
+  return res.json({ status: "OK" });
+});
+
+module.exports = app;
