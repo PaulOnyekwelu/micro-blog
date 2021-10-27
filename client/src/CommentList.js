bcec5d428379186a115d9f774b0680b278246c40
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-10-27 10:27:21 +0100

    code refactoring

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,38 +1,37 @@
-import axios from "axios";
 import React from "react";
 
-// const CommentList = ({ comments }) => {
-//   const renderedComments = comments.map((comment) => {
-//     let content;
-
-//     if (comment.status === "approved") {
-//       content = comment.content;
-//     }
-
-//     if (comment.status === "pending") {
-//       content = "This comment is awaiting moderation";
-//     }
-
-//     if (comment.status === "rejected") {
-//       content = "This comment has been rejected";
-//     }
-
-//     return <li key={comment.id}>{content}</li>;
-//   });
-
-//   return <ul>{renderedComments}</ul>;
-// };
-
 const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
 
-  return (
-    <ul>
-      {comments.length > 0 &&
-        comments.map((comment) => {
-          return <li key={comment.id}>{comment.content}</li>;
-        })}
-    </ul>
-  );
+    if (comment.status === "accepted") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
 };
+
+// const CommentList = ({ comments }) => {
+
+//   return (
+//     <ul>
+//       {comments.length > 0 &&
+//         comments.map((comment) => {
+//           return <li key={comment.id}>{comment.content}</li>;
+//         })}
+//     </ul>
+//   );
+// };
 
 export default CommentList;
--- event-bus/src/app.js
+++ event-bus/src/app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const axios = require("axios");
-const cors = require("cors")
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 app.post("/event", async (req, res) => {
   const data = req.body;
 
-  console.log("event received: ", req.body.type)
-
-  await axios.post("http://localhost:4000/event", data)
-  await axios.post("http://localhost:4001/event", data)
-  await axios.post("http://localhost:4002/event", data)
-  await axios.post("http://localhost:4003/event", data)
+  console.log("event received: ", req.body.type);
+  try {
+    await axios.post("http://localhost:4000/event", data);
+    await axios.post("http://localhost:4001/event", data);
+    await axios.post("http://localhost:4002/event", data);
+    await axios.post("http://localhost:4003/event", data);
+  } catch (error) {
+    console.log(error.message);
+  }
 
   return res.json({ status: "OK" });
 });
