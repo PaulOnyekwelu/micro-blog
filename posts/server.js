8677a930a6a0bf3cac20e27dbeea8235d27e7d17
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-10-29 15:25:39 +0100

    created posts service (nodeport) yaml file

--- posts/server.js
+++ posts/server.js
@@ -5,5 +5,6 @@ const server = http.createServer(app);
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
+  console.log("most recent changess...");
   console.log(`listening on port: ${PORT}`);
 });
